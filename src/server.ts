4e6314246aecb0b464aadf4637489b43db8f1112
talitadzikzlasu <startingcommunity@proton.me>
2025-09-26 17:48:42 +0200

    feat: Add LateRentFlag entity and resolver, register in connectDB and server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {ApolloServer} from 'apollo-server';
 import {buildSchema} from 'type-graphql';
 import ApartmentResolver from './apartment/apartment.resolver';
 import connectDB from './connectDB';
+import LateRentFlagResolver from './lateRentFlag/lateRentFlag.resolver';
 import LeaseResolver from './lease/lease.resolver';
 import PaymentResolver from './payment/payment.resolver';
 import TenantResolver from './tenant/tenant.resolver';
@@ -15,7 +16,7 @@ const PORT = process.env.PORT || 3005;
     await connectDB();
 
     const schema = await buildSchema({
-      resolvers: [ApartmentResolver, TenantResolver, LeaseResolver, PaymentResolver],
+      resolvers: [ApartmentResolver, TenantResolver, LeaseResolver, PaymentResolver, LateRentFlagResolver],
       validate: false,
     });
 
